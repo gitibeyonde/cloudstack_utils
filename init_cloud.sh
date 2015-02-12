b32cdf5527c1867a886c421913d0813df3895fc8
Abhinandan Prateek <abhinandan.prateek@shapeblue.com>
2015-02-12 13:27:50 +0530

    ici

---
+++ init_cloud.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+
+usage() { echo "Usage: $0 [ -c <setup|save|destroy:snapshot> -n <suffix for snapshot> -b <cloudstack branch> ]" 1>&2; exit 1; }
+
+c=unknown
+n=unknown
+while getopts ":c:n:" o; do
+        case "${o}" in
+                c)
+                        c=${OPTARG}
+                        ;;
+                n)
+                        n=${OPTARG}
+                        ;;
+                *)
+                        usage
+                        ;;
+        esac
+done
+
+if [ $c = "unknown" ]; then
+        usage
+elif [ $c = "setup" ]; then
+	#git pull
+  	ssh root@192.168.100.41 git --git-dir=/root/cloudstack_utils/.git --work-tree=/root/cloudstack_utils pull	
+	vmrun start Users/abhinandanprateek/Documents/Virtual\ Machines.localized/ccp.vmwarevm/ccp.vmx
+	vmrun revertToSnapshot /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_2.vmwarevm/Xen65_2.vmx Initial_setup_snap
+	vmrun revertToSnapshot /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_1.vmwarevm/Xen65_1.vmx Initial_setup_snap
+	vmrun start /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_2.vmwarevm/Xen65_2.vmx
+	vmrun start /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_1.vmwarevm/Xen65_1.vmx 
+  	ssh root@192.168.100.40 git --git-dir=/exports/cloudstack/4.5/cloudstack/.git --work-tree=/exports/cloudstack/4.5/cloudstack pull	
+	sleep 120
+	#build
+	ssh root@192.168.100.41 ~/cloudstack_utils/build_cloud.sh
+elif [ $c = "start" ]; then
+	vmrun start Users/abhinandanprateek/Documents/Virtual\ Machines.localized/ccp.vmwarevm/ccp.vmx
+	vmrun start /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_2.vmwarevm/Xen65_2.vmx
+	vmrun start /Users/abhinandanprateek/Documents/Virtual\ Machines.localized/Xen65_1.vmwarevm/Xen65_1.vmx 
+fi
+
